4233a9a52b0f5d37a727fe712d09c04f4f94f066
itsmylifestyle <hahaii00@mail.ru>
2023-01-20 23:24:56 +0600

    Initial Commit

---
+++ MapView/MapViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapViewApp.swift
+//  MapView
+//
+//  Created by Айбек on 20.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct MapViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
